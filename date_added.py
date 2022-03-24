37755832ad0b347c0beb54db49c9f8a99dc69073
Jack Slattery <jack@mgenio.com>
2022-03-24 10:28:30 -0400

    j

--- date_added.py
+++ date_added.py
@@ -32,9 +32,9 @@ def menuPicker(list):
     print(f"{bcolors.OKGREEN}You selected {listStrings[index]}!{bcolors.ENDC}")
     return listStrings[index]
 
-username = input("Enter your username: (slattery.75@osu.edu)") or "slattery.75@osu.edu"
-password = input("Enter your password: (jacksfriendswatchmovies)") or "jacksfriendswatchmovies"
-serverip = input("Enter your server ip: (CRUCIAL_CONTROL_WARD)") or "CRUCIAL_CONTROL_WARD"
+username = "slattery.75@osu.edu" #input("Enter your username: (slattery.75@osu.edu)") or "slattery.75@osu.edu"
+password = "jacksfriendswatchmovies" #input("Enter your password: (jacksfriendswatchmovies)") or "jacksfriendswatchmovies"
+serverip = "CRUCIAL_CONTROL_WARD" #input("Enter your server ip: (CRUCIAL_CONTROL_WARD)") or "CRUCIAL_CONTROL_WARD"
 
 print('Logging you in..')
 
@@ -64,28 +64,28 @@ con = sqlite3.connect('/var/lib/plexmediaserver/Library/Application Support/Plex
 cursor = con.cursor()
 cursor.execute("SELECT added_at FROM metadata_items WHERE title = \""+chosenMovieTitle+"\" and library_section_id = "+chosenLibraryKey)
 print(chosenMovieTitle+"\'s current added date is: "+ cursor.fetchone()[0])
-
-print("Does this look like the correct date?")
-methodIndex = TerminalMenu(["Yes","No"]).show()
-
-if(methodIndex == 0):
-    # cursor.execute("select * from sqlite_master where type = 'trigger';")
-    # triggers = cursor.fetchall()
-    # for trig in triggers:
-    #     print(trig)
-    print("Enter a new DateTime value: ")
-    newDateTime = input()
-    cursor.execute("DROP TRIGGER fts4_metadata_titles_before_update_icu;")
-    cursor.execute("DROP TRIGGER fts4_metadata_titles_after_update_icu;")
-    cursor.execute("UPDATE metadata_items SET added_at = \""+newDateTime+"\" WHERE title = \""+chosenMovieTitle+"\" and library_section_id = "+chosenLibraryKey)
-    cursor.execute("UPDATE metadata_items SET added_at = \""+newDateTime+"\" WHERE title = \""+chosenMovieTitle+"\" and metadata_type = 12 and media_item_count = 3")
-    cursor.execute("SELECT added_at FROM metadata_items WHERE title = \""+chosenMovieTitle+"\" and metadata_type = 12 and media_item_count = 3")
-    print(chosenMovieTitle+"\'s added date is NOW: "+ cursor.fetchone()[0])
-    cursor.execute("CREATE TRIGGER fts4_metadata_titles_before_update_icu BEFORE UPDATE ON metadata_items BEGIN DELETE FROM fts4_metadata_titles_icu WHERE docid=old.rowid; END")
-    cursor.execute("CREATE TRIGGER fts4_metadata_titles_after_update_icu AFTER UPDATE ON metadata_items BEGIN INSERT INTO fts4_metadata_titles_icu(docid, title, title_sort, original_title) VALUES(new.rowid, new.title, new.title_sort, new.original_title); END")
-    con.commit()
-    con.close()
-else:
-    cursor.execute("SELECT added_at FROM metadata_items WHERE title = \""+chosenMovieTitle+"\" AND metadata_type = 12 AND media_item_count = 3")
-    print(chosenMovieTitle+"\'s added date is NOW: "+ cursor.fetchone()[0])
-    print("Bye")
+cursor.execute("SELECT added_at FROM metadata_items WHERE title = \""+chosenMovieTitle+"\" and library_section_id = "+chosenLibraryKey)
+print(chosenMovieTitle+"\'s current added date is: "+ cursor.fetchall()[0])
+#
+# print("Does this look like the correct date?")
+# methodIndex = TerminalMenu(["Yes","No"]).show()
+#
+# if(methodIndex == 0):
+#     # cursor.execute("select * from sqlite_master where type = 'trigger';")
+#     # triggers = cursor.fetchall()
+#     # for trig in triggers:
+#     #     print(trig)
+#     print("Enter a new DateTime value: ")
+#     newDateTime = input()
+#     cursor.execute("DROP TRIGGER fts4_metadata_titles_before_update_icu;")
+#     cursor.execute("DROP TRIGGER fts4_metadata_titles_after_update_icu;")
+#     cursor.execute("UPDATE metadata_items SET added_at = \""+newDateTime+"\" WHERE title = \""+chosenMovieTitle+"\" and library_section_id = "+chosenLibraryKey)
+#     cursor.execute("UPDATE metadata_items SET added_at = \""+newDateTime+"\" WHERE title = \""+chosenMovieTitle+"\" and metadata_type = 12 and media_item_count = 3")
+#     cursor.execute("SELECT added_at FROM metadata_items WHERE title = \""+chosenMovieTitle+"\" and metadata_type = 12 and media_item_count = 3")
+#     print(chosenMovieTitle+"\'s added date is NOW: "+ cursor.fetchone()[0])
+#     cursor.execute("CREATE TRIGGER fts4_metadata_titles_before_update_icu BEFORE UPDATE ON metadata_items BEGIN DELETE FROM fts4_metadata_titles_icu WHERE docid=old.rowid; END")
+#     cursor.execute("CREATE TRIGGER fts4_metadata_titles_after_update_icu AFTER UPDATE ON metadata_items BEGIN INSERT INTO fts4_metadata_titles_icu(docid, title, title_sort, original_title) VALUES(new.rowid, new.title, new.title_sort, new.original_title); END")
+#     con.commit()
+#     con.close()
+# else:
+#     print("Bye")
